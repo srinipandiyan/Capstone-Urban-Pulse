25cff4b7b9d8ab00994c74ec7644491374721207
Srini Pandiyan <srinipandiyan7@gmail.com>
2023-09-19 13:07:47 -0500

    base v1.1

--- app.py
+++ app.py
@@ -8,13 +8,12 @@ app.app_context().push()
 
 from models import db, connect_db
 
-# Get DB_URI from environ variable (useful for production/testing) or,
-# if not set there, use development local db.
-
 app.config['SECRET_KEY'] = "oh-so-secret"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///urban_pulse'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO'] = False
+#uncomment in production
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# set all to false for production/remove echo
+app.config['SQLALCHEMY_ECHO'] = True
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
 # Use shell command to turn on debug mode.
 # $ export FLASK_DEBUG=1 && flask run
