6351663b602170ed24faba2dde8e4d88df2bae55
Srini Pandiyan <srinipandiyan7@gmail.com>
2023-09-20 15:33:14 -0500

    forms for user login, signup, and update

--- app.py
+++ app.py
@@ -2,11 +2,15 @@
 
 from flask import Flask, render_template, request, flash, redirect, session, g, abort
 from flask_debugtoolbar import DebugToolbarExtension
+from sqlalchemy.exc import IntegrityError
+
+from forms import UserAddForm, LoginForm
+from models import db, connect_db, User
+
+CURR_USER_KEY = "curr_user"
 
 app = Flask(__name__)
 app.app_context().push()
-
-from models import db, connect_db
 
 app.config['SECRET_KEY'] = "oh-so-secret"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///urban_pulse'
@@ -21,6 +25,104 @@ toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
 db.create_all()
+
+##############################################################################
+#User signup/login/logout routes
+
+#User is kept track of globally in this route
+#The `@app.before_request` is a decorator that runs a function before all incoming requests to the Flask app. 
+@app.before_request
+def add_user_to_g():
+    """If we're logged in, add curr user to Flask global."""
+    #The g object, short for context globals, is an obj that can keep track of data within the lifetime of a single request.
+    #The following code is used to initialize Flask's g object and store the session user.
+    if CURR_USER_KEY in session:
+        g.user = User.query.get(session[CURR_USER_KEY])
+
+    else:
+        g.user = None
+
+
+def do_login(user):
+    """Log in user."""
+
+    session[CURR_USER_KEY] = user.id
+
+
+def do_logout():
+    """Logout user."""
+
+    if CURR_USER_KEY in session:
+        del session[CURR_USER_KEY]
+
+
+@app.route('/signup', methods=["GET", "POST"])
+def signup():
+    """Handle user signup.
+
+    Create new user and add to DB. Redirect to home page.
+
+    If form not valid, present form.
+
+    If the there already is a user with that username: flash message
+    and re-present form.
+    """
+
+    form = UserAddForm()
+
+    if form.validate_on_submit():
+        try:
+            user = User.signup(
+                username=form.username.data,
+                password=form.password.data,
+                email=form.email.data,
+                image_url=form.image_url.data or User.image_url.default.arg,
+            )
+            db.session.commit()
+
+        except IntegrityError as e:
+            flash("Username already taken", 'danger')
+            return render_template('users/signup.html', form=form)
+
+        do_login(user)
+
+        return redirect("/")
+
+    else:
+        return render_template('users/signup.html', form=form)
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    """Handle user login."""
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.authenticate(form.username.data,
+                                 form.password.data)
+
+        if user:
+            do_login(user)
+            flash(f"Hello, {user.username}!", "success")
+            return redirect("/")
+
+        flash("Invalid credentials.", 'danger')
+
+    return render_template('users/login.html', form=form)
+
+
+@app.route('/logout')
+def logout():
+    """Handle logout of user."""
+    #user = User.query.get(session[CURR_USER_KEY])
+    session.pop(CURR_USER_KEY)
+    flash("User is logged out.", "success")
+    #f"{user.username} is logged out.""
+    return redirect("/")
+
+
+
 
 @app.route("/")
 def root():
---
+++ forms.py
@@ -0,0 +1,29 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SelectField, TextAreaField
+from wtforms.validators import DataRequired, Email, Length
+
+
+class UserAddForm(FlaskForm):
+    """Signup form."""
+
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[Length(min=6)])
+
+
+
+class LoginForm(FlaskForm):
+    """Login form."""
+
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[Length(min=6)])
+
+
+class UpdateUserForm(FlaskForm):
+    """Update user profile and preferences form."""
+
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[Length(min=6)])
+    base_city = SelectField('Select Base City:', remove_option=True, coerce=int)
+    occupation = SelectField('Select Occupation:', remove_option=True, coerce=int)
