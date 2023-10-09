2bb4cd2da5aaf86e828547a577cab7ef68284e9f
Srini Pandiyan <125408728+srinipandiyan@users.noreply.github.com>
2023-10-09 00:26:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ app = Flask(__name__)
 app.app_context().push()
 
 app.config['SECRET_KEY'] = "some_secret_key"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///urban_pulse'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
