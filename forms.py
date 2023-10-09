f384a130dae4e6c60434d52fd4f2ad631900acb0
Srini Pandiyan <125408728+srinipandiyan@users.noreply.github.com>
2023-10-09 18:53:37 -0500

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SelectField, TextAreaField
+from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired, Email, Length
 
 
