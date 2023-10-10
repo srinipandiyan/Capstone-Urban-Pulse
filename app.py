30e6e43bb5a47a31709e8456392e6c2327376eaf
Srini Pandiyan <125408728+srinipandiyan@users.noreply.github.com>
2023-10-09 19:38:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 """Flask app for Urban Pulse"""
-
+import os
 from flask import Flask, render_template, flash, redirect, request, session, g
-from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy.exc import IntegrityError
 import requests
 
@@ -14,18 +13,21 @@ CURR_USER_KEY = "curr_user"
 app = Flask(__name__)
 app.app_context().push()
 
-app.config['SECRET_KEY'] = "some_secret_key"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///urban_pulse'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    os.environ.get('DATABASE_URL', 'postgresql:///urban_pulse'))
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
+
+connect_db(app)
 
 #app.config['SQLALCHEMY_ECHO'] = True
 # Use shell command to turn on debug mode.
 # $ export FLASK_DEBUG=1 && flask run
+#from flask_debugtoolbar import DebugToolbarExtension
 #toolbar = DebugToolbarExtension(app)
-
-connect_db(app)
-db.create_all()
 
 ##############################################################################
 #homepage/signup/login/logout routes with error handling
